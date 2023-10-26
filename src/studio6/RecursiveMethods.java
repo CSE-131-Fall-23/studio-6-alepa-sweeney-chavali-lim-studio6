f963cebad34ec54da903eecf496c12ffcd2adf17
rahulchavali <rahulchavali@localhost>
2023-10-26 12:47:27 -0500

    finished

--- src/studio6/RecursiveMethods.java
+++ src/studio6/RecursiveMethods.java
@@ -12,9 +12,10 @@ public class RecursiveMethods {
 	 *         ...)
 	 */
 	public static double geometricSum(int n) {
-		
+	
 			// FIXME compute the geometric sum for the first n terms recursively
-			return 0;
+		
+			return 0.5 + n + geometricSum(n/2);
 		
 	}
 
